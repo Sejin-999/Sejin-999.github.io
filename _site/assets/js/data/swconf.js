3bb6f03bd1d29947a6fbcfad5f1808f225df8ac3
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-03-19 21:39:59 +0900

    test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710851829',resources: [
+    cacheName: 'chirpy-1710851985',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
