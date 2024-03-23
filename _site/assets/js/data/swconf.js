66e68be7d2a49ecb18f7abc410462b90f43a4527
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-03-23 14:53:29 +0900

    스레드

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711051375',resources: [
+    cacheName: 'chirpy-1711173196',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
